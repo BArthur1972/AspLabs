204e5b8d166b820f69ad9e1ad81c08ca02c4ac6e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-06-07 11:33:24 +0100

    Fix loading webassembly app

--- src/QuickGrid/samples/QuickGridSamples.Server/Program.cs
+++ src/QuickGrid/samples/QuickGridSamples.Server/Program.cs
@@ -1,45 +1,50 @@
-using Microsoft.AspNetCore.ResponseCompression;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace Company.WebApplication1
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapRazorPages();
+app.MapControllers();
+
+ApplyHotReloadWorkaround(app);
+
+app.Map("/webassembly", app =>
+{
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+    app.UseEndpoints(endpoints => endpoints.MapFallbackToFile("index.html"));
+});
+
+app.Run();
+
+// This is a temporary workaround needed until the next patch release of ASP.NET Core
+// The underlying bug was fixed in https://github.com/dotnet/sdk/pull/25534 but that update hasn't shipped yet
+void ApplyHotReloadWorkaround(WebApplication app)
+{
+    app.Use((ctx, next) =>
     {
-        public static void Main(string[] args)
+        if (ctx.Request.Path == "/webassembly/_framework/blazor-hotreload")
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-
-            app.MapRazorPages();
-            app.MapControllers();
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
+            ctx.Response.Redirect("/_framework/blazor-hotreload");
+            return Task.CompletedTask;
         }
-    }
+
+        return next(ctx);
+    });
 }
